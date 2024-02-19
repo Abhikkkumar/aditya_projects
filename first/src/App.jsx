dbacb95123881ecf8dcb7a040a372bd1bc95c8d6
Abhishek Kumar <imakumarabhishek@gmail.com>
2024-02-19 20:30:38 +0530

    first

---
+++ first/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Card_1 from './components/Card_1'
+
+function App() {
+ 
+  return (
+    <div>
+      <Card_1 />
+
+    </div>
+  )
+}
+
+export default App
---
+++ first/src/components/Card_1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Card_1() {
+  return (
+    <div>
+      card 1
+      
+    </div>
+  )
+}
